933ca69777894bc43e5c604284b90dc7137033c9
Moheeb <egoist@mailbox.org>
2021-01-07 09:16:01 -0600

    Fully Implemented Ray, file exported checks out.

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,48 @@ use vec3::color::*;
 use vec3::ray::*;
 
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+
+
+    let aspect_ratio:f32 = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f32 / aspect_ratio) as i32;
+
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+
+    let focal_length: f32 = 1.0;
+
+    let origin = Point3{e: [0.00,0.00,0.00]};
+    let horizontal = Vec3 {e: [viewport_width,0.00,0.00]};
+    let vertical = Vec3 {e: [0.00,viewport_height,0.00]};
+
+    let lower_left_corner = origin - horizontal/2.00 - vertical/2.00 -
+        Vec3{e: [0.00, 0.00, focal_length]};
+
+
+
+
+
+
+
 
         println!("P3\n{} {}\n255", image_width, image_height);
         for j in (0..image_height).rev(){
             for i in 0..image_width{
-
-                let pixel_color: Color = Color::new(i as f32 / (image_width-1) as f32,
-                                                    j as f32 / (image_height-1) as f32,
-                                                    0.25);
-
+                let u = i as f32 / (image_width-1) as f32;
+                let v = j as f32 / (image_height-1) as f32;
+                let r = Ray{
+                    origin: origin,
+                    direction: lower_left_corner + horizontal*u + vertical*v - origin,
+                };
+                let pixel_color: Color = ray_color(r);
                 write_color(pixel_color);
-
 
             }
         }
 
 
-    vec3_tester();
+    //vec3_tester();
 
 
 
@@ -60,4 +83,14 @@ fn vec3_tester(){
     };
 
     println!("{:?}", r1.at(1.00));
+
+}
+
+
+
+fn ray_color(r: Ray) -> Color {
+    let unit_direction = r.direction.unit_vector();
+    let t = (unit_direction.y() + 1.0) * 0.5;
+    return Color{e: [1.0, 1.0, 1.0]} * (1.0-t) + Color{e: [0.5, 0.7, 1.0]}*t ;
+
 }
